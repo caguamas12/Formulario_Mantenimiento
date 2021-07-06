550b80d7ff19a2c8e39c9b54502384afd397d57c
jose Ignacio <caguamas12@gmail.com>
2021-07-06 17:10:22 -0500

    primeros pasos

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="estilo%20form.css">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <div id="contenedor">
+        <form>
+            <label for="name">Nombre:</label>
+            <input type="text" id="name" name="name"><br>
+            <label for="apellido">Apellido:</label>
+            <input type="text" id="apellido" name="apellido"><br>
+            <label for="asunto">Asunto:</label>
+            <input type="text" id="asunto" name="asunto"><br>
+            <label for="Mensaje/detalles">Mensaje/Detalles</label>
+            <input type="text" id="mensaje" name="mensaje">
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
+<?php
